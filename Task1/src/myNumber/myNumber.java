eaf62b5aacd7dc4fe50eb627a9edeb527b99ab4a
MarioGera <130042180+MarioGera@users.noreply.github.com>
2023-10-15 22:55:56 +0200

    Add files via upload

---
+++ Task1/src/myNumber/myNumber.java
@@ -0,0 +1,28 @@
+package myNumber;
+
+public class myNumber {
+private int num;
+public int num2;
+public myNumber() {
+num=0 ;
+num2=100;
+}
+int getNum() {
+return num;
+}
+void SetNum(int i) {
+num=i;
+num2=10*i;
+}
+
+
+public class myclass {
+public static void main (String[]ar) {
+myNumber A=new myNumber();
+System.out.println(A.getNum());
+A.SetNum(10);
+System.out.println(A.getNum());
+System.out.println(A.num2);
+}
+}
+}
